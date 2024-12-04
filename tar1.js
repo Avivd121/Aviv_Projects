eb00530206a524b93eacec1cdaa5a7130d51daf2
Avivd121 <130778737+Avivd121@users.noreply.github.com>
2024-12-04 17:31:25 +0200

    Update tar1.js

--- tar1.js
+++ tar1.js
@@ -1,4 +1,6 @@
-// task 1a
+//Presenter: Aviv Dahan
+//task 1a
+//The program checks If the number is divisible by 2 or 3 or 5 – print 1 (divisible by only one number).
 const num1 = 123;
 Number(num1 % 5 === 0) &&
   Number(num1 % 2 === 0) &&
@@ -6,11 +8,13 @@ Number(num1 % 5 === 0) &&
   console.log("1");
 
 //task 1b
+//The program checks if the number is also divisible by 2 and 3 or by 3 and 5 or by 2 and 5 – print 2 (divisible by only two numbers).
 const num2 = 123;
 (Number(num2 % 2 === 0) && Number(num2 % 3 === 0) && console.log("2")) ||
   (Number(num2 % 3 === 0) && Number(num2 % 5 === 0) && console.log("2")) ||
   (Number(num2 % 2 === 0) && Number(num2 % 5 === 0) && console.log("2"));
 
-// tar 1c
+//task 1c
+//The program checks if the number is also divisible by 2, 3, and 5 – print 3.
 const num = 123;
 num % 5 === 0 && num % 2 === 0 && num % 3 === 0 && console.log("3");
